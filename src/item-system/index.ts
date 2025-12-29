607ce18dff136efb5f0c58a4f054986fdb853ea2
aljames-arctic <bakanabaka.vtt@gmail.com>
2025-12-29 15:24:57 -0600

    cleanup: function naming
    Signed-off-by: aljames-arctic <bakanabaka.vtt@gmail.com>

--- src/item-system/index.ts
+++ src/item-system/index.ts
@@ -1,18 +1,16 @@
 import { calculateUsesForItem as dnd5eCalculateUsesForItem } from './dnd5e';
 
-const getItemSystem = () => {
+const getItemSystemCalculator = () => {
   switch (game.system.id) {
     case 'dnd5e':
-      return dnd5eCalculateUsesForItem;
+        return dnd5eCalculateUsesForItem;
     default:
-      return null;
+        return null;
   }
 };
 
 export const calculateUsesForItem = (item: dnd5e.documents.Item5e) => {
-  const calculateUsesForItemSystem = getItemSystem();
-  if (!calculateUsesForItemSystem) {
-    return null;
-  }
-  return calculateUsesForItemSystem(item);
+  const itemSystemCalculator = getItemSystemCalculator();
+  if (!itemSystemCalculator) { return null; }
+  return itemSystemCalculator(item);
 };
